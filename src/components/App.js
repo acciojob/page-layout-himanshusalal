bf7d86e209953134fec7245c3024a602ff6235ab
himanshusalal <himanshusalal517@gmail.com>
2026-05-11 12:51:02 +0530

    start

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import './../styles/App.css';
 import PageLayout from "../PageLayout";
@@ -6,19 +5,16 @@ import PageLayout from "../PageLayout";
 const App = () => {
   return (
     <div>
+      {/* Do not remove the main div */}
+
       <PageLayout
-        /* Do not remove the main div */
-        //header={<h3>Welcome to my website</h3>}
-
-        footer={<p>© 2023 My Website. All right reserved.</p>}
-        >
-
-       <h1>Welcome to my website</h1>
-        <p>This is the content of my website</p>
-
-        </PageLayout>
+        header={<h1>Welcome to my website</h1>}
+        footer={<p>© 2023 My Website. All rights reserved.</p>}
+      >
+        <p>This is the content of my website.</p>
+      </PageLayout>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
